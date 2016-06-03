a098c4eff1c23c3d5e468ff4d0ad4bed0d213f75
ShMcK <shawn.j.mckay@gmail.com>
2016-06-02 19:11:59 -0700

    improve route menu link names

--- src/components/AppMenu/menuRightRouteOptions.tsx
+++ src/components/AppMenu/menuRightRouteOptions.tsx
@@ -1,13 +1,24 @@
 import * as React from 'react';
 import MenuLink from './MenuLink';
 
-const routes = [
-  'tutorialConfig',
-  'tutorialInfo',
-  'page'
-];
+const routes = [{
+    route: 'tutorialConfig',
+    name: 'config'
+  }, {
+    route: 'tutorialInfo',
+    name: 'info'
+  }, {
+    route: 'page',
+    name: 'edit'
+  }];
 
-// return all routes in list except route provided
 export default function menuRightRouteOptions(route: string) {
-  return routes.filter(r => route !== r).map(r => <MenuLink route={r} />);
+  // return all routes in list except route provided
+  return routes.filter(r => route !== r.route).map(r => (
+    <MenuLink
+      route={r.route}
+      title={r.name}
+    />
+    )
+  );
 }
