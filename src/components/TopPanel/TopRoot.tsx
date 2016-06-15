8d52a7df1f4f4f51f50d2dc180657ee9e86b1a93
ShMcK <shawn.j.mckay@gmail.com>
2016-06-14 18:18:12 -0700

    refactor top/side root render methods

--- src/components/TopPanel/TopRoot.tsx
+++ src/components/TopPanel/TopRoot.tsx
@@ -1,18 +1,14 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import store from '../../store';
 import TopPanel from './TopPanel';
 import muiTheme from '../styles/theme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-export default function render(target: HTMLElement) {
-  ReactDOM.render(
-      <Provider store={store}>
-        <MuiThemeProvider muiTheme={muiTheme}>
-          <TopPanel />
-        </MuiThemeProvider>
-      </Provider>,
-    target
-  );
-}
+const TopRoot = store => (
+  <Provider store={store}>
+    <MuiThemeProvider muiTheme={muiTheme}>
+      <TopPanel />
+    </MuiThemeProvider>
+  </Provider>
+);
+export default TopRoot;
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 import * as React from 'react';
-import {sideElement} from './components/SidePanel';
-import render from './components/render';
-import {topElement} from './components/TopPanel';
-import renderTop from './components/TopPanel/TopRoot';
-import {loadPolyfills} from 'core-coderoad';
+import * as ReactDOM from 'react-dom';
+import {sideElement, SideRoot} from './components/SidePanel';
+import {topElement, TopRoot} from './components/TopPanel';
+import {loadPolyfills, render} from 'core-coderoad';
 import {onActivate, onDeactivate} from './subscriptions';
 import store from './store';
 import {setupVerify} from './modules/setup';
+import * as injectTapEventPlugin from 'react-tap-event-plugin';
 
 class Main {
   side: HTMLElement;
   top: HTMLElement;
   constructor() {
+    // remove later
+    injectTapEventPlugin();
+
     loadPolyfills();
     // run startup checks
     store.dispatch(setupVerify());
@@ -30,8 +33,9 @@ class Main {
     });
     onActivate();
     // render React component
-    render(this.side);
-    renderTop(this.top);
+    // render(this.side);
+    ReactDOM.render(SideRoot(store), this.side);
+    ReactDOM.render(TopRoot(store), this.top);
   }
   deactivate(): void {
     // remove subscriptions & unmount react app
